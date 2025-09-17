a490d20726e1a6b8f844a0cac7fbd25ea43027ed
Obaro Awowede <akpobaroawowede@gmail.com>
2025-09-17 13:27:06 +0100

    Created a correlated subquery tolist users who have made more than 3 bookings

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -6,3 +6,12 @@ WHERE Property.property_id IN (
     GROUP BY Property.property_id
     HAVING AVG(Review.rating) > 4.0
 );
+
+SELECT User.user_id, User.first_name, Booking.booking_id, Booking.status
+FROM `User` 
+JOIN Booking ON User.user_id = Booking.user_id
+WHERE (
+    SELECT COUNT(*)
+    From Booking
+    WHERE User.user_id = Booking.user_id
+) > 3;
